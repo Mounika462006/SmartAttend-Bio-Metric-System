45b9af91a3a093b002fcebb3d810f05e6a612f84
Dharunkumar <dharunkumarsengottuvelu@gmail.com>
2026-06-26 22:25:46 +0530

    feat: configure Vercel deployment by adding serverless function entry point and build script

--- Backend/api/index.js
+++
@@ -1,4 +0,0 @@
-const app = require('../src/app');
-
-// Vercel serverless functions expect the Express app to be exported directly
-module.exports = app;
---
+++ api/index.js
@@ -0,0 +1,4 @@
+const app = require('../Backend/src/app');
+
+// Expose the Express app as a Vercel Serverless Function
+module.exports = app;
